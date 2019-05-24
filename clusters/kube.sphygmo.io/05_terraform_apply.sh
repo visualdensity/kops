2780abf0a4238b5df4b2d829d04151d39c19ef02
Wee Keat Chin <weekeat@visualdensity.com>
2019-05-24 01:06:43 +0000

    Nicer to have state

--- clusters/kube.sphygmo.io/05_terraform_apply.sh
+++ clusters/kube.sphygmo.io/05_terraform_apply.sh
@@ -12,5 +12,5 @@ fi
 
 echo "You can use this to check instances if you don't have a lot of instances :)"
 echo
-echo "  aws ec2 describe-instances | jq -r '.Reservations[].Instances[] | { PrivateIP: .PrivateIpAddress, Type: .SecurityGroups[0].GroupName }'"
+echo "  aws ec2 describe-instances | jq -r '.Reservations[].Instances[] | { PrivateIP: .PrivateIpAddress, Type: .SecurityGroups[0].GroupName, State: .State.Name }'"
 echo
